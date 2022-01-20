6598af320126b0acf2e25d0b832fad983e7e634e
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-20 13:24:44 +0100

    test /hello?name

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -20,17 +20,17 @@ suite('Functional Tests', function () {
           done();
         });
     });
-//     // #2
-//     test('Test GET /hello with your name', function (done) {
-//       chai
-//         .request(server)
-//         .get('/hello?name=xy_z')
-//         .end(function (err, res) {
-//           assert.fail(res.status, 200);
-//           assert.fail(res.text, 'hello xy_z');
-//           done();
-//         });
-//     });
+    // #2
+    test('Test GET /hello with your name', function (done) {
+      chai
+        .request(server)
+        .get('/hello?name=xy_z')
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xy_z');
+          done();
+        });
+    });
 //     // #3
 //     test('Send {surname: "Colombo"}', function (done) {
 //       chai
